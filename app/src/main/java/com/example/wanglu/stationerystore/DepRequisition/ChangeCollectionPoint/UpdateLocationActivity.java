e93d9b0079c6d9f6c67727c3bc04423ec0a3c9e6
Anthony <luo18chao@gmail.com>
2018-07-19 10:51:03 +0800

    Modified updateLocationActivity

--- app/src/main/java/com/example/wanglu/stationerystore/DepRequisition/ChangeCollectionPoint/UpdateLocationActivity.java
+++ app/src/main/java/com/example/wanglu/stationerystore/DepRequisition/ChangeCollectionPoint/UpdateLocationActivity.java
@@ -8,7 +8,11 @@ import android.widget.RadioButton;
 
 import com.example.wanglu.stationerystore.R;
 
+import java.util.ArrayList;
+
 public class UpdateLocationActivity extends AppCompatActivity {
+    ArrayList<String> nameList=new ArrayList<String>(){{add("pointA");add("pointB");add("pointC");add("pointD");add("pointD");}};
+    ArrayList<String> numberList=new ArrayList<String>(){{add("1");add("2");add("3");add("4");add("5");}};
 
     private ConstraintLayout collectionPoints=null;
 
@@ -21,6 +25,18 @@ public class UpdateLocationActivity extends AppCompatActivity {
 
         collectionPoints =  findViewById(R.id.collectionPoints);//initiate include(include ID is collectionPoints)
         //initiate buttons
+
+        confirmButton= findViewById(R.id.confirmbutton);
+        //load text to radioButton
+        int[] ids={R.id.collectPoint1RadioButton,R.id.collectPoint2RadioButton,R.id.collectPoint3RadioButton,R.id.collectPoint4RadioButton,R.id.collectPoint5RadioButton};
+        for(int i=0;i<nameList.size();i++)
+        {
+            String text=nameList.get(i);
+            RadioButton r=(RadioButton)findViewById(ids[i]);
+            r.setText(text);
+        }
+
+
 
 
     }
