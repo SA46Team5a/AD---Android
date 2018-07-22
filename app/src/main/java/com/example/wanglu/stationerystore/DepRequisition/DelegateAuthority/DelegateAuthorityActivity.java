3bbc4a4257459d180442c0094bbb266909765a8f
Anthony <luo18chao@gmail.com>
2018-07-22 11:40:13 +0800

    added button to handle datepicker dialog

--- app/src/main/java/com/example/wanglu/stationerystore/DepRequisition/DelegateAuthority/DelegateAuthorityActivity.java
+++ app/src/main/java/com/example/wanglu/stationerystore/DepRequisition/DelegateAuthority/DelegateAuthorityActivity.java
@@ -4,20 +4,31 @@ import android.app.DatePickerDialog;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.wanglu.stationerystore.Fragments.DatePickerFragment;
 import com.example.wanglu.stationerystore.R;
 
+import java.lang.reflect.Array;
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class DelegateAuthorityActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
     private ConstraintLayout delegateLayout=null;
+    TextView startText;
+    TextView endText;
+    Button targetBtn;
+    String selectedEmpName;
+    String selectedEmpId;
 
     @Override
     public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
@@ -26,17 +37,24 @@ public class DelegateAuthorityActivity extends AppCompatActivity implements Date
         c.set(Calendar.MONTH,month);
         c.set(Calendar.DAY_OF_MONTH,dayOfMonth);
         String currentDateString= DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime());
-        EditText d=findViewById(R.id.enddate);
-        d.setText(currentDateString);
+        if()
+        if(targetBtn.getId()==R.id.startButton)
+        {
+            startText.setText(currentDateString);
+        }
+        if(targetBtn.getId()==R.id.endButton)
+        {
+            endText.setText(currentDateString);
+        }
+
     }
     //create touchEventListener class for multiple touch event use for start/end date editTextView
-    class TouchListener implements View.OnTouchListener{
+    class ClickListener implements View.OnClickListener{
         @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
+        public void onClick(View view) {
             android.support.v4.app.DialogFragment datePicker=new DatePickerFragment();
+             targetBtn= (Button) view;
             datePicker.show(getSupportFragmentManager(),"date picker");
-            //((EditText)view).setText();
-            return false;
         }
     }
 
@@ -45,18 +63,50 @@ public class DelegateAuthorityActivity extends AppCompatActivity implements Date
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delegate_form);
 
+        startText=findViewById(R.id.startdate);
+        endText=findViewById(R.id.enddate);
         delegateLayout=findViewById(R.id.delegateInclude);
-        EditText d=findViewById(R.id.enddate);
-
-
-        d.setOnTouchListener(new TouchListener());
-        Button b=findViewById(R.id.confirmButton);
-        b.setOnClickListener(new View.OnClickListener() {
+        Button startBtn=findViewById(R.id.startButton);
+        Button endBtn=findViewById(R.id.endButton);
+        startBtn.setOnClickListener(new ClickListener());
+        endBtn.setOnClickListener(new ClickListener());
+        Button confirmBtn=findViewById(R.id.confirmButton);
+//        if(true)
+//        {
+//            startBtn.setEnabled(false);
+//        }
+        confirmBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Toast.makeText(DelegateAuthorityActivity.this,selectedEmpId,Toast.LENGTH_SHORT).show();
 
 
+            }
+        });
+        //load dropdownlist
+        final ArrayList<String> emplist=new ArrayList<String>()
+        {
+            {
+                add("empA");add("empB");add("empC");add("empD");
+            }
+        };
+        final ArrayList<String> empIDlist=new ArrayList<String>()
+        {
+            {
+                add("1");add("2");add("3");add("4");
+            }
+        };
 
+        Spinner empDropdownlist = findViewById(R.id.authoritySpinner);
+
+        final ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, emplist);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        empDropdownlist.setAdapter(adapter );
+        empDropdownlist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                selectedEmpName= adapterView.getItemAtPosition(i).toString();
+                empIDlist.get( emplist.indexOf(selectedEmpName));
             }
         });
 
