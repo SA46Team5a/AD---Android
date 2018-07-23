2a9a95db82b60d70eaa2fee1457247e3f7989814
Anthony <luo18chao@gmail.com>
2018-07-23 13:27:34 +0800

    added DisbursementListDepActivity and disbursementListDeptAdapter

--- app/src/main/java/com/example/wanglu/stationerystore/StoreRequisition/ConfirmDisbursementList/DisbursementListDeptActivity.java
+++ app/src/main/java/com/example/wanglu/stationerystore/StoreRequisition/ConfirmDisbursementList/DisbursementListDeptActivity.java
@@ -1,15 +1,25 @@
 package com.example.wanglu.stationerystore.StoreRequisition.ConfirmDisbursementList;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
+import com.example.wanglu.stationerystore.Adapter.DisbursementListDeptAdapter;
 import com.example.wanglu.stationerystore.R;
 
 public class DisbursementListDeptActivity extends AppCompatActivity {
+    private ConstraintLayout disbursementInclude=null;
+    ListView itemsListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_disbersment_confirm);
+        disbursementInclude=findViewById(R.id.disbursementInclude);
+        itemsListView=findViewById(R.id.itemsListView);
+        DisbursementListDeptAdapter adapter=new DisbursementListDeptAdapter(this);
+        itemsListView.setAdapter(adapter);
+
     }
 }
