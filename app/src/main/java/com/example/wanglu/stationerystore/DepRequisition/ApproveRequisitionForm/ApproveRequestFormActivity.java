372b59dd76f660d051ccb53a335cb404480e098b
Jack <jsim89@gmail.com>
2018-08-02 15:36:56 +0800

    feat(ApproveRequestFormActivity): show toast if there are no outstanding requisitions

--- app/src/main/java/com/example/wanglu/stationerystore/DepRequisition/ApproveRequisitionForm/ApproveRequestFormActivity.java
+++ app/src/main/java/com/example/wanglu/stationerystore/DepRequisition/ApproveRequisitionForm/ApproveRequestFormActivity.java
@@ -4,16 +4,17 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.wanglu.stationerystore.Adapter.ApproveRequestAdapter;
 import com.example.wanglu.stationerystore.Model.ApproveRequestModel;
@@ -56,10 +57,15 @@ public class ApproveRequestFormActivity extends AppCompatActivity {
             @Override
             protected void onPostExecute(HashMap<String,ArrayList<String>> result) {
                 approvaMap =result;
-                Log.i("Size", String.valueOf(result.size()));
-                ApproveRequestAdapter adapter = new ApproveRequestAdapter(ApproveRequestFormActivity.this);
-                ListView itemlistview = findViewById(R.id.Listview) ;
-                itemlistview.setAdapter(adapter);
+                if (result.get("RequisitionID").size() == 0) {
+                    Toast.makeText(ApproveRequestFormActivity.this, "There are no outstanding requisitions", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Log.i("Size", String.valueOf(result.size()));
+                    ApproveRequestAdapter adapter = new ApproveRequestAdapter(ApproveRequestFormActivity.this);
+                    ListView itemlistview = findViewById(R.id.Listview) ;
+                    itemlistview.setAdapter(adapter);
+                }
             }
 
         }.execute();
